dbd61cdc478d26aab5ec25f667e8b95405def981
Rich Hart <RichHOHart@gmail.com>
2013-08-13 10:07:20 -0400

    String token example

--- read_file_example.c
+++ read_file_example.c
@@ -3,25 +3,44 @@
  
 int main()
 {
-   char ch, file_name[25];
-   FILE *fp;
+
+	int sz;
+	char ch;
+	char filein_name[81] = "mesh_and_color_surface.iv";
+	char read_data[] = "one two three";
+   FILE * fp;
  
-   printf("Enter the name of file you wish to see\n");
-   gets(file_name);
- 
-   fp = fopen(file_name,"r"); // read mode
+   fp = fopen(filein_name,"r"); // read mode
  
    if( fp == NULL )
    {
       perror("Error while opening the file.\n");
       exit(EXIT_FAILURE);
    }
- 
-   printf("The contents of %s file are :\n", file_name);
- 
-   while( ( ch = fgetc(fp) ) != EOF )
-      printf("%c",ch);
- 
+ 	fseek(fp, 0L, SEEK_END);
+	sz = ftell(fp);
+   printf("The contents of %s file are :\n", filein_name);
+printf("Size of file  is %d \n", sz);
+ fseek(fp, 0L, SEEK_SET);
+
+//read_data= (char*)malloc(sz*sizeof(char));
+
+int index = 0; 
+   while( ( ch = fgetc(fp) ) != EOF ){
+	read_data[index]=ch;
+	index=index+1;
+ }
+ fseek(fp, 0L, SEEK_SET);
+
+
+
+char* token = strtok(read_data, " ");
+while (token) {
+    printf("token: %s\n", token);
+    token = strtok(NULL, " ");
+}
+
+free(read_data);
    fclose(fp);
    return 0;
 }
